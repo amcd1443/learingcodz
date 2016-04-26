80606b77473eefbde0823218f78199f12b6400d6
Austin duer <brash@Austins-MacBook-Air.local>
2016-04-26 00:52:35 -0400

    commiting array hw

---
+++ basics/array.js
@@ -0,0 +1,22 @@
+var arrayXmen = [
+	"cyclops",
+	"jean grey",
+	"gambit",
+	"wolverine",
+	"jubilee",
+	"rogue",
+	"beast",
+	"professor x",
+	"morph",
+	"storm"
+];
+
+var totalNumberOfTeam = arrayXmen.length
+console.log(totalNumberOfTeam)
+
+var  alphabeticalOfTeam = arrayXmen.sort()
+console.log(alphabeticalOfTeam)
+
+var teamLeaders = arrayXmen[0] + " " + arrayXmen[9] //why cant i do arrayXmen[0,9]?
+console.log(teamLeaders)
+

e5756757ccf83b21ec747137ca20c314b77010ae
Austin duer <brash@Austins-MacBook-Air.local>
2016-05-04 12:51:08 -0400

    all cleaned up

--- babel/translator.js
+++ babel/translator.js
@@ -37,85 +37,6 @@ var frenchDictionary = {
 	"my": "mon"
 };
 
-var translateOneFrenchSentence = function(frenchPhraseToTranslate){
-	var frenchWordsArray = frenchPhraseToTranslate.split(" ");
-	var frenchTranslatedSentence = "";
-	var counter = 0;
-	while (counter < frenchWordsArray.length) {
-		var thisOriginalWord = frenchWordsArray[counter];
-		var translatedFrench = translateOneWord(thisOriginalWord);
-		if (counter > 0) {
-			frenchTranslatedSentence = frenchTranslatedSentence + " ";
-		}
-		frenchTranslatedSentence = frenchTranslatedSentence + translatedFrench;
-		counter = counter + 1;
-	}
-
-	return frenchTranslatedSentence
-};
-
-
-
-var translateThisEnglishSentenceToGerman = function(germanPhraseToTranslate) {
-	var germanWordsArray = germanPhraseToTranslate.split(" ");
-	var germanTranslatedSentence = "";
-	var counter = 0;
-	while (counter < germanWordsArray.length) {
-		var thisOriginalWord = germanWordsArray[counter];
-		var translatedGerman = translateEnglishToGerman(thisOriginalWord);
-		if (counter > 0) {
-			germanTranslatedSentence = germanTranslatedSentence + " " ;
-		}
-		germanTranslatedSentence = germanTranslatedSentence + translatedGerman;
-		counter = counter + 1;
-	}
-
-	return germanTranslatedSentence	
-};
-
-
-var translateOneSentenceToSpanish = function(phraseToTranslate) {   
-		var arrayOfWordsInOriginalSentence = phraseToTranslate.split(" ");
-		var translatedSentence = "";
-		//
-		var counter = 0; 
-		while(counter < arrayOfWordsInOriginalSentence.length) {
-			var thisOriginalWord = arrayOfWordsInOriginalSentence[counter];
-			var translatedWord = translateOneWordToSpanish(thisOriginalWord);
-			if (counter > 0) {
-				translatedSentence = translatedSentence + " " ;
-			}
-			translatedSentence = translatedSentence + translatedWord;
-
-			counter = counter + 1; 
-		}
-
-		return translatedSentence;
-};
-
-
-
-
-
-
-
-
-var translateEnglishToGerman = function(englishWord) {
-	var germanWord = germanDictionary[englishWord];
-	if (germanWord == null) {
-		return englishWord;
-	} else {
-	 	return germanWord;
-	}	
-};
-
-var translateOneWordToSpanish = function(wordToTranslate) {
-	var translatedWord = spanishDictionary[wordToTranslate];
-	if (translatedWord == null) {
-		return wordToTranslate;
-	};
-	return translatedWord;	
-};
 
 
 var translateOneWordToAnyLanguage = function(word, language) {
@@ -157,10 +78,4 @@ var translateOneSentenceToAnyLanguage = function(sentence, language) {
 };
 
 
-
-
-module.exports.translateOneWordToSpanish = translateOneWordToSpanish;
-module.exports.translateOneSentenceToSpanish = translateOneSentenceToSpanish;
-module.exports.translateEnglishToGerman = translateEnglishToGerman;
-module.exports.translateThisEnglishSentenceToGerman = translateThisEnglishSentenceToGerman;
 module.exports.translateOneSentenceToAnyLanguage = translateOneSentenceToAnyLanguage;

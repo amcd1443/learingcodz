fe750e18c2ee33b7ae3cbaf2a9fa3fb9953697b9
Austin <mcclainduer@gmail.com>
2018-01-09 19:07:04 -0500

    randomized first player, added in game winner text, and tie game text

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -2,19 +2,29 @@
 var playersTurn = document.getElementById('playersTurn'); //playersTurn equals the HTML ID playersTurn
 var xScore = document.getElementById('xScore');
 var oScore = document.getElementById('oScore');
-
+var random = ((Math.random() * 10) + 1);
 var isXTurn = true;
-
 var xTotalPoints = 0;
 var oTotalPoints = 0;
-
 var currentWinner = null;
+var totalClicks = null;
 
 
+window.onload = console.log(random);
+
+var randomlyChoosePlayer = function()	{
+	if (random < 5)	{
+		playersTurn.innerHTML = "Player's X Turn";
+	} else {
+		playersTurn.innerHTML = "Player's O Turn";
+	}
+};
+randomlyChoosePlayer();
+
+/* //The original clickHandler, keeping for record
 var clickHandler = function() { //start of function that handles how clicking will work
-
 	if ( (this.innerHTML == "X") || (this.innerHTML == "O") || (currentWinner != null) ) {	//if innerHTML = X or O or currentWinner does NOT equal null, return out of the function
-		console.log("blocking click");
+		//blocks the double click
 		return;	//return means exit out of the function ASAP.
 	}
 	this.style.backgroundColor = 'blue'; //this specific variable (to clickHandler) 
@@ -23,15 +33,65 @@ var clickHandler = function() { //start of function that handles how clicking wi
 	playersTurn.innerHTML = "Player O's turn"; //changes text in playersTurn to Player O's turn
 
 	if (isXTurn == false) {	//if global variable isXTurn is false run the following code
-		this.innerHTML = "O";	//change innerHTML of clicked object to 0 (zero)
+		this.innerHTML = "O";	//change innerHTML of clicked object to O 
 		this.style.backgroundColor = 'orange';	//change backgroundColor to Orange
 		playersTurn.innerHTML = "Player X's turn"; //change text to Player Xs Turn
 	};
+	
 	isXTurn = !isXTurn;	//IF isXTurn true make false, ELSE make false true
+	checkIfWinner();
+	scoreTracker();	
+};
+*/
 
+var clickHandlerX = function() {
+	if ( (this.innerHTML == "X") || (this.innerHTML == "O") || (currentWinner != null) ) {
+		return;
+	}
+	this.style.backgroundColor = 'blue';
+	this.innerHTML = "X";
+	playersTurn.innerHTML = "Player O's Turn";
+	console.log("it was X's turn, ")
+	if (isXTurn == false) {
+		this.innerHTML = "O";
+		this.style.backgroundColor = 'orange';
+		playersTurn.innerHTML = "Player X's Turn";
+	}
+	isXTurn = !isXTurn;
 	checkIfWinner();
 	scoreTracker();
+	totalClicks++;
+	console.log(totalClicks, " = totalClicks X");
+	if (totalClicks >= 8) {
+		checkIfTie();
+		console.log("checkIfTie initiate");
+	}
 };
+
+var clickHandlerO = function() {
+	if ( (this.innerHTML == "X") || (this.innerHTML == "O") || (currentWinner != null) ) {
+		return;
+	}
+	this.style.backgroundColor = 'orange';
+	this.innerHTML = "O";
+	playersTurn.innerHTML = "Player X's Turn";
+	if (isXTurn == false) {
+		this.innerHTML = "X";
+		this.style.backgroundColor = 'blue'
+		playersTurn.innerHTML = "Player O's Turn";
+	}
+	isXTurn = !isXTurn;
+	checkIfWinner();
+	scoreTracker();
+	totalClicks++;
+	console.log(totalClicks, " = totalClicks O");
+	if (totalClicks >= 8) {
+		checkIfTie();
+		console.log("checkIfTie initiate");
+	}
+};
+
+
 
 var checkIfWinner = function() {
 	var box0 = document.getElementById('box0');
@@ -44,9 +104,9 @@ var checkIfWinner = function() {
 	var box7 = document.getElementById('box7');
 	var box8 = document.getElementById('box8');
 
-	var stringBase = "better way for ";
-	var stringTail = " to win";	
-	var displayString = null;
+	var stringBase = "Player ";
+	var stringTail = " won. Game over, play again?";	
+	var displayString = null; //why did we make it null?
 	//X horizontal wins
 	if ( (box0.innerHTML == box1.innerHTML) && (box0.innerHTML == box2.innerHTML) ) { 	//this says that what ever innerHTML is set to will check the,
 		displayString = stringBase + box0.innerHTML + stringTail;			//other two boxes if they match, equals win
@@ -68,7 +128,7 @@ var checkIfWinner = function() {
 		currentWinner = box0.innerHTML;
 	}
 	if ( (box2.innerHTML == box4.innerHTML) && (box2.innerHTML == box6.innerHTML) ) {
-		playersTurn.innerHTML = stringBase + box2.innerHTML + stringTail;
+		displayString = stringBase + box2.innerHTML + stringTail;
 		currentWinner = box2.innerHTML;
 	}
 
@@ -78,11 +138,11 @@ var checkIfWinner = function() {
 		currentWinner = box0.innerHTML;
 	}
 	if ( (box1.innerHTML == box4.innerHTML) && (box1.innerHTML == box7.innerHTML) ) {
-		playersTurn.innerHTML = stringBase + box1.innerHTML + stringTail;
+		displayString = stringBase + box1.innerHTML + stringTail;
 		currentWinner = box1.innerHTML;	
 	}
 	if ( (box2.innerHTML == box5.innerHTML) && (box2.innerHTML == box8.innerHTML) ) {
-		playersTurn.innerHTML = stringBase+ box2.innerHTML + stringTail;
+		displayString = stringBase+ box2.innerHTML + stringTail;
 		currentWinner = box2.innerHTML;
 	}
 
@@ -91,10 +151,14 @@ var checkIfWinner = function() {
 	}
 };
 
+var checkIfTie = function() {
+		playersTurn.innerHTML = "Tied Game!!! Try again?";
+		console.log("its a tie!!");
+};
 
 
 
-var scoreTracker = function()	{ //function that keeps score between O and X
+var scoreTracker = function()	{	//function that keeps score between O and X
 	if (currentWinner == "O") { 	//this states that if playersTurn text is "netter way for O to win" run the if statement
 		oTotalPoints++
 		oScore.innerHTML = oTotalPoints;
@@ -108,8 +172,8 @@ var scoreTracker = function()	{ //function that keeps score between O and X
 	}
 
 };
-//also reset the player's turn text too, after you solve the scoring issue
-var resetGameArea = function()	{
+
+var resetGameArea = function()	{	//function that resets the game area to begin a new game
 	for (var i = 0; i < 9; i++) {
 		var box = document.getElementById("box" + i);
 		box.innerHTML = i;
@@ -117,26 +181,31 @@ var resetGameArea = function()	{
 		
 	};
 	currentWinner = null;
+	totalClicks = 0;	//why was making this null, adding to the scoreboard?
 };
 
-var resetScoreBoard = function() {
+var resetScoreBoard = function() {	//function that resets the scorebaord so new people can play
 	document.getElementById('oScore').innerHTML = 0;
 	document.getElementById('xScore').innerHTML = 0;
 	resetGameArea();
 };
 
-for (var i = 0; i < 9; i++) { 
+for (var i = 0; i < 9; i++) {		//for loop that numbers out the boxes for the clickHandler functions
 	var box = document.getElementById("box" + i);	//var box is box+(what ever i is = to starts 0 ends 8)
 	box.style.cursor = 'pointer';	//changes the style of the mouse cursor to a pointer
-	box.onclick = clickHandler;	//when you click a box it runs the clickHandler function above
-
+	if (random < 5) {
+		box.onclick = clickHandlerX;
+	} else {
+		box.onclick = clickHandlerO;
+	}
+	//box.onclick = clickHandler 	//when you click a box it runs the clickHandler function above
 };
 
 //Homework
-
-// fix playersTurn text area for game over - 
-// to randomly choose X or O that goes first use math.Random
-// if its a tie game say so and ¿auto-reset?
+// fix playersTurn text area for game over - done.
+// to randomly choose X or O that goes first use math.Random - done
+// if its a tie game say so and ¿auto-reset? - how
+// 
 
 
 

996a17ab8ecb0168c2c910421f338a15732372b6
Austin <mcclainduer@gmail.com>
2018-01-27 16:36:37 -0500

    adding chess to game folder

---
+++ game/chess.js
@@ -0,0 +1,24 @@
+for (var i = 0; i < 9; i++) {
+	var squareA = document.getElementById("squareA" + i);
+	var squareB = document.getElementById("squareB" + i);
+	var squareC = document.getElementById("squareC" + i);
+	var squareD = document.getElementById("squareD" + i);
+	var squareE = document.getElementById("squareE" + i);
+	var squareF = document.getElementById("squareF" + i);
+	var squareG = document.getElementById("squareG" + i);
+	var squareH = document.getElementById("squareH" + i);
+	//squareA.innerHTML = 'this shold work'
+	squareA.style.cursor = 'pointer';
+	// squareB.style.cursor = 'pointer';
+	// squareC.style.cursor = 'pointer';
+	// squareD.style.cursor = 'pointer';
+	// squareE.style.cursor = 'pointer';
+	// squareF.style.cursor = 'pointer';
+	// squareG.style.cursor = 'pointer';
+	// squareH.style.cursor = 'pointer';
+};
+ 
+
+
+
+console.log(squareA, i);
